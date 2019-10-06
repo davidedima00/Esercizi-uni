606aeb22b6cdabe4f75e89595362a1bacc4c6a4b
davidedima00 <56118845+davidedima00@users.noreply.github.com>
2019-10-06 13:51:19 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,93 +1,43 @@
 /*
-Author: Davide Di marco
-Date: 04/10/2019
-Note: Il seguente programma stampa a video il valore minimo, massimo delle variabili l'occupazione in memoria
-      per ogni tipo di dato ed infine il numero di caratteri stampati da ogni funzione printf
-
+author: Davide Di marco
+date: 06/10/2019
+notes:
 */
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <float.h>
+#include <math.h>
 
 int main()
 {
+    /*Utilizzo degli operatori aritmetici*/
 
-    /*Dichiaro e inizializzo tutti i tipi di variabili possibili con CHAR*/
-    char carattere='$';
-    unsigned char uChar=184;
-    signed char sChar=-26;
+    int a=10;
+    int b=5;
+    float c=2.1678;
+    int addizione,sottrazione,divisione,moltiplicazione,modulo;
 
+    addizione=a+b; //operatore +
+    sottrazione=a-(-b);  //operatore - e operatore inversione di segno di b
+    divisione=a/b;   //operatore /
+    moltiplicazione=a*b; //operatore *
+    modulo=a%b; //restituisce il resto della divisione tra a e b
 
-   /*Dichiaro e inizializzo tutti i tipi di variabili possibili con INT*/
-    int vInt=1045;
-    unsigned int uInt=12456;
-    signed int sInt=-25;
+    float divisioneF;
+    divisioneF=-c/b; // il risultato della divisione in questo caso ha la virgola e con il segno di c
+    printf("%f",divisioneF);
+    a++; //incrementa di 1 il valore di a
+    b--; //decrementa di 1 il valore di b
+    a+=2; //incrementa di 2 il valore di a
+    a-=2;//decrementa di 2 il valore di a
+    a*=2; // moltiplica per 2 a e assegna ad a il risultato
+    a/=2; //divide per 2 a e assegna ad a il risultato
+    a%=2; //ottiene il resto della divisione a/2 e assegna ad a il risultato ottenuto
+    b=a++; //assegna a b il valore di a e poi incrementa di 1 il valore di a
+    b=a--; //assegna a b il valore di a e poi decrementa di 1 il valore di a
 
-    /*Dichiaro e inizializzo tutti i tipi di variabili possibili con SHORT*/
-    short int shortInt=10;
-    unsigned short int usInt=1254;
-    signed short int ssInt=-82;
-
-    /*Dichiaro e inizializzo tutti i tipi di variabili possibili con LONG*/
-    long int lInt=37894657;
-    unsigned long int ulInt =12345699;
-    signed long int slInt= -2067894558;
-
-
-    /*Dichiaro e inizializzo tutti i tipi di variabili possibili con DOUBLE*/
-    float vFloat=1.2134541345f;
-    double vDouble=1.256789178f;
-
-
-    int carStampati; //dichiaro una variabile per inserire allinterno i caratteri stampati dal printf
-
-    /*STAMPO LE COMBINAZIONI PER IL TIPO CHAR*/
-    carStampati=printf("TYPE: %-19s | VALUE: %15c | ALLOCATED: %2d byte | MAX_VALUE: %012d | MIN_VALUE: %012d","CHAR",carattere,sizeof(char),CHAR_MAX,CHAR_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15c | ALLOCATED: %2d byte | MAX_VALUE: %12d | MIN_VALUE: %12d","UNSIGNED CHAR",uChar,sizeof(unsigned char),UCHAR_MAX,0);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15c | ALLOCATED: %2d byte | MAX_VALUE: %12d | MIN_VALUE: %12d","SIGNED CHAR",sChar,sizeof(signed char),SCHAR_MAX,SCHAR_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15d | ALLOCATED: %2d byte | MAX_VALUE: %12d | MIN_VALUE: %12d","INT",vInt,sizeof(int),INT_MAX,INT_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15u | ALLOCATED: %2d byte | MAX_VALUE: %12u | MIN_VALUE: %12d","UNSIGNED INT",uInt,sizeof(unsigned int),UINT_MAX,0);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15i | ALLOCATED: %2d byte | MAX_VALUE: %12d | MIN_VALUE: %12d","SIGNED INT",sInt,sizeof(signed int),INT_MAX,INT_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15d | ALLOCATED: %2d byte | MAX_VALUE: %12d | MIN_VALUE: %12d","SHORT INT",shortInt,sizeof(short int),SHRT_MAX,SHRT_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15u | ALLOCATED: %2d byte | MAX_VALUE: %12u | MIN_VALUE: %12u","UNSIGNED SHORT INT",usInt,sizeof(unsigned short int),USHRT_MAX,0);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15d | ALLOCATED: %2d byte | MAX_VALUE: %12d | MIN_VALUE: %12d","SIGNED SHORT INT",ssInt,sizeof(signed short int),SHRT_MAX,SHRT_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15ld | ALLOCATED: %2d byte | MAX_VALUE: %12ld | MIN_VALUE: %12ld","LONG INT",lInt,sizeof(long int),LONG_MAX,LONG_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15lu | ALLOCATED: %2d byte | MAX_VALUE: %12lu | MIN_VALUE: %12d","UNSIGNED LONG INT",ulInt,sizeof(unsigned long int),ULONG_MAX,0);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %15ld | ALLOCATED: %2d byte | MAX_VALUE: %12ld | MIN_VALUE: %12ld","SIGNED LONG INT",slInt,sizeof(signed long int),LONG_MAX,LONG_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-
-    carStampati=printf("TYPE: %-19s | VALUE: %16.8f| ALLOCATED: %2d byte | MAX_VALUE: %12.4e | MIN_VALUE: %12.4e","FLOAT",vFloat,sizeof(float),FLT_MAX,FLT_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
-
-    carStampati=printf("TYPE: %-19s | VALUE: %16.8lf| ALLOCATED: %2d byte | MAX_VALUE: %12.4e | MIN_VALUE: %12.4e","DOUBLE",vDouble,sizeof(double),DBL_MAX,DBL_MIN);
-    printf(" | CHARS PRINTED: %-3d\n",carStampati);
 
     return 0;
 }
